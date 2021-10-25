c68ee44f951faa683874455f8b1f057e5329511f
JYG <jojeayun900@gmail.com>
2021-10-25 21:19:34 +0900

    최종 수정 및 js 적용

--- JS/fullPage.js
+++ JS/fullPage.js
@@ -1,31 +1,7 @@
-//기존 이벤트 제거
-window.addEventListener("wheel", function(e){
-	e.preventDefault();
-},{passive : false});
-
-//페이지 선언
-var $html = $("html");
-var page = 1;
-var lastPage = $(".wich").length;
-
-$html.animate({scrollTop:0},10);
-
-//마우스 휠 굴렸을때 이벤트 넣기
-$(window).on("wheel", function(e){
- 
-	if($html.is(":animated")) return;
- 
-	if(e.originalEvent.deltaY > 0){
-		if(page== lastPage) return;
- 
-		page++;
-	}else if(e.originalEvent.deltaY < 0){
-		if(page == 1) return;
- 
-		page--;
+window.onload=async()=>{
+	alert("저희 웹사이트에 오신것을 환영해요!");
+	var check2= await confirm("들어오실건가요?");
+	if(!check2){
+		location.href="https://www.google.co.kr/";
 	}
-	var posTop = (page-1) * $(window).height();
- 
-	$html.animate({scrollTop : posTop});
- 
-});
+};
